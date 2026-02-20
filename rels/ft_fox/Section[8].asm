9d848ec2197d641b415b497e7104f3ffff5c15a2
djpvstv <65921378+djpvstv@users.noreply.github.com>
2026-02-19 19:22:39 -0500

    fox clone rel
    Almost works, still have issues with the landmaster

---
+++ rels/ft_fox/Section[8].asm
@@ -0,0 +1,86 @@
+loc_0:
+        .4byte 0x00000055
+        .4byte 0xCCCCCCCC
+        .4byte 0xCCCCCCCC
+        .4byte 0xCCCCCCCC
+        .4byte 0xCCCCCCCC
+        .4byte 0xCCCCCCCC
+        .4byte 0xCCCCCCCC
+        .4byte 0xCCCCCCCC
+        .4byte 0x00000000         [R_PPC_ADDR32(27, 1, "loc_148380")]
+        .4byte 0x00000000         [R_PPC_ADDR32("ft_fox", 8, "loc_80")]
+        .4byte 0x00000018
+        .4byte 0x00000D8A
+        .4byte 0x00000000         [R_PPC_ADDR32(27, 1, "loc_148414")]
+        .4byte 0x00000000         [R_PPC_ADDR32("ft_fox", 8, "loc_A0")]
+        .4byte 0x00000018
+        .4byte 0x00000D8A
+        .4byte 0x00000000         [R_PPC_ADDR32(27, 1, "loc_1484A8")]
+        .4byte 0x00000000         [R_PPC_ADDR32("ft_fox", 8, "loc_C0")]
+        .4byte 0x00000018
+        .4byte 0x00000D8A
+        .4byte 0x00000000         [R_PPC_ADDR32(27, 1, "loc_85E06C")]
+        .4byte 0x00000000         [R_PPC_ADDR32("ft_fox", 8, "loc_110")]
+        .4byte 0x00000024
+        .4byte 0x00000D8A
+        .4byte 0x00000000
+        .4byte 0x00000000         [R_PPC_ADDR32("ft_fox", 8, "loc_E0")]
+        .4byte 0x00000009
+        .4byte 0x00001D8A
+        .4byte 0xCCCCCCCC
+        .4byte 0xCCCCCCCC
+        .4byte 0xCCCCCCCC
+        .4byte 0xCCCCCCCC
+loc_80:
+        .4byte 0x80630000
+        .4byte 0x1C84000C
+        .4byte 0x7C63202E
+        .4byte 0x8063009C
+        .4byte 0x4E800020
+        .4byte 0x4BFFFFEC
+        .4byte 0xCCCCCCCC
+        .4byte 0xCCCCCCCC
+loc_A0:
+        .4byte 0x80630000
+        .4byte 0x1C84000C
+        .4byte 0x7C63202E
+        .4byte 0x806300A0
+        .4byte 0x4E800020
+        .4byte 0x4BFFFFEC
+        .4byte 0xCCCCCCCC
+        .4byte 0xCCCCCCCC
+loc_C0:
+        .4byte 0x80630000
+        .4byte 0x1C84000C
+        .4byte 0x7C63202E
+        .4byte 0x806300A4
+        .4byte 0x4E800020
+        .4byte 0x4BFFFFEC
+        .4byte 0xCCCCCCCC
+        .4byte 0xCCCCCCCC
+loc_E0:
+        .4byte 0x00000000         [R_PPC_ADDR32("ft_fox", 1, "loc_A0" + 2)]
+        .4byte 0x00000000         [R_PPC_ADDR32("ft_fox", 1, "loc_168" + 2)]
+        .4byte 0x00000000         [R_PPC_ADDR32("ft_fox", 1, "loc_17F8" + 2)]
+        .4byte 0x00000000         [R_PPC_ADDR32("ft_fox", 1, "loc_9830" + 2)]
+        .4byte 0x00000000         [R_PPC_ADDR32("ft_fox", 1, "loc_9A48" + 2)]
+        .4byte 0x00000000         [R_PPC_ADDR32("ft_fox", 1, "loc_9ADC" + 2)]
+        .4byte 0x00000000         [R_PPC_ADDR32("ft_fox", 1, "loc_E6C4" + 2)]
+        .4byte 0x00000000         [R_PPC_ADDR32("ft_fox", 1, "loc_E72C" + 2)]
+        .4byte 0x00000000         [R_PPC_ADDR32("ft_fox", 1, "loc_E788" + 2)]
+        .4byte 0xCCCCCCCC
+        .4byte 0xCCCCCCCC
+        .4byte 0xCCCCCCCC
+loc_110:
+        .4byte 0x5480103A
+        .4byte 0x3C6080B9
+        .4byte 0x80635320
+        .4byte 0x7C630214
+        .4byte 0x80630154
+        .4byte 0x2C030000
+        .4byte 0x4C820020
+        .4byte 0x38800006
+        .4byte 0x4BFFFFE0
+        .4byte 0xCCCCCCCC
+        .4byte 0xCCCCCCCC
+        .4byte 0xCCCCCCCC
